b5a652d6b5088692f2b8a6e21af272c46b207ffe
Liam Keene <liam.keene@gmail.com>
2013-05-12 20:45:27 +1000

    Order now implements a Parcelable
     * modified src/com/liamkeene/orderpicker/Order.java - by implementing Parcelable, the
    	Order class be flattened and passed in Intents

--- src/com/liamkeene/orderpicker/Order.java
+++ src/com/liamkeene/orderpicker/Order.java
@@ -1,12 +1,15 @@
 package com.liamkeene.orderpicker;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class Order {
-    String id;
-    String name;
-    String date;
+public class Order implements Parcelable {
+    private String id;
+    private String name;
+    private String date;
 
     Order(String id, String name, String date) {
         // Explicity set the attributes
@@ -28,5 +31,39 @@ public class Order {
 
     public String toString() {
         return "Order " + this.id;
+    }
+
+    @Override
+    public int describeContents() {
+        // Describe special contents
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        // Flatten the attributes that we require to a Parcel
+        dest.writeString(id);
+        dest.writeString(name);
+        dest.writeString(date);
+    }
+
+    public static final Parcelable.Creator<Order> CREATOR
+            = new Parcelable.Creator<Order>() {
+        // Creator that generates instances of Orders from a parcel
+        public Order createFromParcel(Parcel in) {
+            return new Order(in);
+        }
+
+        public Order[] newArray(int size) {
+            return new Order[size];
+        }
+    };
+
+    private Order(Parcel in) {
+        // Constructor to create an Order from a Parcel
+        // Attributes are read in the order they were written to the Parcel
+        id = in.readString();
+        name = in.readString();
+        date = in.readString();
     }
 }
