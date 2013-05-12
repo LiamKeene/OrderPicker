baf8a3c6e23d40cf9227b31db1e182b2ca7e5153
Liam Keene <liam.keene@gmail.com>
2013-05-12 17:34:24 +1000

    Successfully integrated AsyncTask to fetch latest Orders and display
     * modified AndroidManifest.xml - internet permission
     * modified src/com/liamkeene/orderpicker/Order.java - constructor for JSONObject -> Order
     * modified src/com/liamkeene/orderpicker/OrderListFragment.java - created an inner class
    	LatestOrdersAsyncTask, which asynchronously fetches the latest orders from our API, 
    	and adds them to the ListView (in the ListFragment)

--- src/com/liamkeene/orderpicker/Order.java
+++ src/com/liamkeene/orderpicker/Order.java
@@ -1,17 +1,32 @@
 package com.liamkeene.orderpicker;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Order {
     String id;
     String name;
     String date;
 
     Order(String id, String name, String date) {
+        // Explicity set the attributes
         this.id = id;
         this.name = name;
         this.date = date;
     }
 
-    public String toDisplay() {
+    Order(JSONObject jsonObject) {
+        // Set attributes from JSONObject - no type checking
+        try {
+            this.id = jsonObject.getString("id");
+            this.name = jsonObject.getString("name");
+            this.date = jsonObject.getString("date");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String toString() {
         return "Order " + this.id;
     }
 }
--- src/com/liamkeene/orderpicker/OrderListFragment.java
+++ src/com/liamkeene/orderpicker/OrderListFragment.java
@@ -2,20 +2,43 @@ package com.liamkeene.orderpicker;
 
 import android.app.ListFragment;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.liamkeene.orderpicker.Order;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class OrderListFragment extends ListFragment {
+    //
+    private List<String> ordersArray = new ArrayList<String>();
+    private ArrayAdapter<String> arrayAdapter;
+
     // Implement the OrderList class
     private final String[] orderList = {"Order 1", "Order 2", "Order 3", };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setListAdapter(new ArrayAdapter(getActivity().getApplicationContext(),
-            android.R.layout.simple_list_item_activated_1, orderList));
+
+        // Get the latest orders asynchronously
+        new LatestOrdersAsyncTask().execute();
     }
 
     @Override
@@ -32,6 +55,58 @@ public class OrderListFragment extends ListFragment {
             startActivity(launchingIntent);
         } else {
             viewer.populateOrderDetails(orderList[position]);
+        }
+    }
+
+    private class LatestOrdersAsyncTask extends AsyncTask<String, Void, String>
+    {
+        @Override
+        protected void onPreExecute() {
+            // Any code to execute before fetching the data
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String response = "";
+
+            DefaultHttpClient client = new DefaultHttpClient();
+            HttpGet httpGet = new HttpGet("http://wms.liamkeene.com/api/orders");
+
+            try {
+                HttpResponse execute = client.execute(httpGet);
+                InputStream content = execute.getEntity().getContent();
+
+                BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                String s = "";
+                while ((s = buffer.readLine()) != null) {
+                    response += s;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                // Create a JSON array
+                JSONArray jsonOrders = new JSONArray(result);
+
+                // Get each JSON Object and add to the array
+                for (int i=0; i < jsonOrders.length(); i++) {
+                    // Create an Order
+                    Order order = new Order(jsonOrders.getJSONObject(i));
+                    ordersArray.add(order.toString());
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            // Create an adapter and add the orders to it
+            arrayAdapter = new ArrayAdapter<String>(
+                getActivity(), android.R.layout.simple_list_item_1, ordersArray
+            );
+            setListAdapter(arrayAdapter);
         }
     }
 }
