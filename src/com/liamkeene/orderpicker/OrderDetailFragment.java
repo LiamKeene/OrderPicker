8a070655ee8808a8a93a6161b531be40d1fa864d
Liam Keene <liam.keene@gmail.com>
2013-05-12 21:13:47 +1000

    Added some inputs for the full Order fields
     * modified res/layout/order_detail_fragment.xml - added TextViews for other attributes
     * modified res/values/strings.xml
     * modified src/com/liamkeene/orderpicker/OrderDetailFragment.java - set the new TextViews

--- src/com/liamkeene/orderpicker/OrderDetailFragment.java
+++ src/com/liamkeene/orderpicker/OrderDetailFragment.java
@@ -12,7 +12,9 @@ import com.liamkeene.orderpicker.Order;
 
 public class OrderDetailFragment extends Fragment {
     // Implement the Order class
-    TextView orderText;
+    TextView textOrderID;
+    TextView textOrderName;
+    TextView textOrderDate;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -21,12 +23,18 @@ public class OrderDetailFragment extends Fragment {
         Order order = launchingIntent.getParcelableExtra("com.liamkeene.orderpicker.Order");
         View viewer = (View) inflater.inflate(
             R.layout.order_detail_fragment, container, false);
-        orderText = (TextView) viewer.findViewById(R.id.label_order_name);
+
+        textOrderID = (TextView) viewer.findViewById(R.id.label_order_id);
+        textOrderName = (TextView) viewer.findViewById(R.id.label_order_name);
+        textOrderDate = (TextView) viewer.findViewById(R.id.label_order_date);
+
         populateOrderDetails(order);
         return viewer;
     }
 
     public void populateOrderDetails(Order order) {
-        orderText.setText(order.toString());
+        textOrderID.setText(order.getId());
+        textOrderName.setText(order.getName());
+        textOrderDate.setText(order.getDate());
     }
 }
